24dfa18d16b183e71f757678eabc3f9881c78af9
Jordi Dalmau <jordidalmau.alum@insestatut.cat>
2025-01-19 14:37:39 +0100

    add country insertion page: enhance UI with country list and actions for modification and deletion

--- gestio-peliculas/insertPais.php
+++ gestio-peliculas/insertPais.php
@@ -19,12 +19,39 @@ $paises = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <h1>Añadir Pais</h1>
 
-<form action="insertar_pais.php" method="POST">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+        <form action="insertar_pais.php" method="POST">
     <label for="nombrePais">Nombre del Pais:</label>
     <input type="text" id="nombrePais" name="nombrePais" required><br>
 
     <input type="submit" value="Añadir Pais">
 </form>
 Volver al <a href="index.php">listado de actores</a>
+        </div>
+        <div class="col-md-6">
+            <h2>Lista de paises</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre del Pais</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($paises as $pais): ?>
+                        <tr>
+                            <td><?php echo $pais['nombrePais']; ?></td>
+                            <td><a href="modificarPais.php?id=<?php echo $pais['idPais']; ?>">Modificar</a> | <a href="eliminarPais.php?id=<?php echo $pais['idPais']; ?>">Eliminar</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
 </body>
 </html>

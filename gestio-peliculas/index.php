fcc73931dece8ada420489021e5fe6c61a6ae8ae
Jordi Dalmau <jordidalmau.alum@insestatut.cat>
2025-01-19 11:59:42 +0100

    add country management: update actor list to include country names and enhance HTML structure

--- gestio-peliculas/index.php
+++ gestio-peliculas/index.php
@@ -1,4 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestió Películas | Jordi Dalmau</title>
+</head>
+<body>
 <h1>Gestió Películas</h1>
+<p>Col·lecció de dades filmogràfiques</p>
 
 <?php
 
@@ -8,6 +17,7 @@ $conn = connectBD();
 ?>
 
 <a href="insertActor.php">CREAR UN NUEVO ACTOR</a>
+<a href="insertPais.php">AÑADE UN PAIS</a>
 <table>
 <thead>
         <tr>
@@ -20,22 +30,28 @@ $conn = connectBD();
 <tbody>
 <?php
 try {
-    $sql = "SELECT * FROM actor";
-    $listaActores = $conn->query($sql);
+    $sql = "SELECT actor.*, pais.nombrePais 
+            FROM actor 
+            JOIN pais ON actor.nacionalidadActor = pais.idPais";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $listaActores = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    while ($actor = $listaActores->fetch(PDO::FETCH_ASSOC)) {
+    foreach ($listaActores as $actor) {
         echo "
             <tr>
-                <td><img src={$actor['imagen']} alt='Imagen del actor' style='width: 100px; height: auto;'></td>
-                <td>{$actor['nacionalidadActor']}</td>
+                <td><img src='{$actor['imagen']}' alt='Imagen del actor' style='width: 100px; height: auto;'></td>
+                <td>{$actor['nombrePais']}</td>
                 <td>{$actor['nombreActor']}</td>
                 <td><a href='modificarActor.php?id={$actor['idActor']}'>Modificar</a> | <a href='eliminarActor.php?id={$actor['idActor']}'>Eliminar</a></td>
             </tr>
         ";
     }
-} catch(PDOExcepcion $excepcion) {
+} catch(PDOException $excepcion) {
     echo "Error en la consulta de tipo " . $excepcion->getMessage();
 }
 ?>
 </tbody>
 </table>
+</body>
+</html>
